21a353b21aa711abe538ef237939724d1d5770ca
Borrubasz <pawello99@o2.pl>
2021-05-13 14:47:16 +0200

    Improved classifier

--- src/ImgMachineLearning.py
+++ src/ImgMachineLearning.py
@@ -1,14 +1,15 @@
 from ImgPatchExtractor import ImgPatchExtractor
 from ImgReader import ImgReader
-from sklearn import neighbors
+from sklearn import tree
 from imblearn import metrics
 import numpy as np
 import os
+import pickle
 
 
 class ImgMachineLearning:
     def __init__(self, reader: ImgReader):
-        self.__classifier = neighbors.KNeighborsClassifier(n_neighbors=10, weights='distance', n_jobs=-1)
+        self.__classifier = tree.DecisionTreeClassifier(random_state=0)
         self.__reader = reader
         self.__patch = ImgPatchExtractor(reader)
 
@@ -25,27 +26,18 @@ class ImgMachineLearning:
         files = self.__get_files_list()
 
         all_train = [[], []]
-        all_test = [[], []]
 
-        for file in files[:10]:
+        for file in ["01_dr", "01_g", "01_h", "02_g"]:
             self.__reader.load_image(file, "../img/")
 
             self.__patch.extract_patches()
 
-            train, test = self.__patch.get_train_test_sets()
+            train = self.__patch.get_train_sets()
 
             all_train[0].extend(train[0])
             all_train[1].extend(train[1])
-            all_test[0].extend(test[0])
-            all_test[1].extend(test[1])
 
         self.__classifier.fit(all_train[0], all_train[1])
-
-        predictions = self.__classifier.predict(all_test[0])
-        predictions_train = self.__classifier.predict(all_train[0])
-
-        print(metrics.classification_report_imbalanced(all_test[1], predictions))
-        print(metrics.classification_report_imbalanced(all_train[1], predictions_train))
 
     def classify(self, filename: str, path: str):
         self.__reader.load_image(filename, path)
@@ -59,5 +51,13 @@ class ImgMachineLearning:
 
             for j in range(len(coords)):
                 new_img[coords[j][0], coords[j][1]] = predictions[j]
-
+            print("Zakończyłem ćwiartkę {}".format(i))
         return new_img
+
+    def load_classifier(self, filename: str, path: str):
+        with open(path + filename, 'rb') as file:
+            self.__classifier = pickle.load(file)
+
+    def dump_classifier(self, filename: str, path: str):
+        with open(path + filename, 'wb') as file:
+            pickle.dump(self.__classifier, file)
--- src/ImgPatchExtractor.py
+++ src/ImgPatchExtractor.py
@@ -1,11 +1,10 @@
 import numpy as np
 from sklearn.feature_extraction import image
-from sklearn.model_selection import train_test_split
-from imblearn.under_sampling import RandomUnderSampler
 import skimage.measure as measure
 import skimage.filters as filters
 import skimage.exposure as exposure
 from ImgReader import ImgReader
+import random as r
 
 
 class ImgPatchExtractor:
@@ -16,15 +15,14 @@ class ImgPatchExtractor:
         self.__patches = np.array([])
 
     def __create_indices_matrix(self):
-        tmp = np.indices(self.__indices.shape[0:2], sparse=True)
         for i in range(self.__indices.shape[0]):
             for j in range(self.__indices.shape[1]):
-                self.__indices[i, j, 0] = tmp[0][i, 0]
-                self.__indices[i, j, 1] = tmp[1][0, j]
+                self.__indices[i, j, 0] = i
+                self.__indices[i, j, 1] = j
 
     def set_reader(self, reader: ImgReader):
         self.__reader = reader
-        self.__indices = np.indices(self.__reader.get_img().shape)
+        self.__indices = np.indices(self.__reader.get_img().shape)  # __indices should probably be initialized with np.zeroes not np.indices, check this
 
     def extract_patches(self):
         self.__patches = image.extract_patches_2d(self.__indices, (5, 5), max_patches=20000)
@@ -41,50 +39,28 @@ class ImgPatchExtractor:
 
         self.__patches = self.__patches[filter_arr]
 
-    def get_train_test_sets(self):
-        y = []
+    def get_train_sets(self):
+        y_set = []
+        x_set = []
         expert_mask = self.__reader.get_standard()
         img = self.__reader.get_img()[:, :, 1]
 
-        img = filters.gaussian(img)
-        img = filters.unsharp_mask(img)
-        img = exposure.equalize_hist(img)
+        for i in range(2000000):
+            x = r.randint(2, 3501)
+            y = r.randint(2, 2333)
+            x_set.append(img[y-2:y+3, x-2:x+3].flatten())
+            y_set.append(expert_mask[y, x])
 
-        x_moments = []
+        x_set = np.array(x_set)
+        y_set = np.array(y_set)
 
-        for i in self.__patches:
-            target = 1 if expert_mask[i[2, 2, 0], i[2, 2, 1]] > 0 else 0
-            y.append(target)
-
-            tmp_img = img[i[0, 0, 0]:i[4, 4, 0]+1, i[0, 0, 1]:i[4, 4, 1]+1]
-            norm_moments = measure.moments_normalized(measure.moments_central(tmp_img))
-
-            hu_moments = measure.moments_hu(norm_moments)
-            hu_moments = np.where(hu_moments != 0, hu_moments, 10**-50)
-
-            for j in range(len(hu_moments)):
-                hu_moments[j] = -1 * np.copysign(1.0, hu_moments[j]) * np.log10(abs(hu_moments[j]))
-
-            parameters = np.append(hu_moments, np.var(tmp_img))
-
-            x_moments.append(parameters)
-
-        x_moments = np.array(x_moments)
-        y = np.array(y)
-
-        x_train, x_test, y_train, y_test = train_test_split(x_moments, y, test_size=0.33)
-
-        test = (x_test, y_test)
-
-        resampler = RandomUnderSampler(sampling_strategy=1.0)
-
-        train = resampler.fit_resample(x_train, y_train)
-
-        return train, test
+        return x_set, y_set
 
     def get_quarter_patches(self, quarter: int):
         if 4 < quarter < 1:
             raise Exception("Wrong quarter chosen: should be between 1 and 4")
+
+        parameters = []
 
         shp = self.__indices.shape
 
@@ -96,40 +72,11 @@ class ImgPatchExtractor:
         end = quarter * shp[0] // 4
 
         patches = image.extract_patches_2d(self.__indices[start:end], (5, 5))
-        mask = self.__reader.get_mask()
-        filter_tab = []
-
-        for i in patches:
-            if mask[i[2, 2, 0], i[2, 2, 1]] == 1:
-                filter_tab.append(True)
-            else:
-                filter_tab.append(False)
-
-        patches = patches[filter_tab]
-        patches = patches[::2]
 
         img = self.__reader.get_img()[:, :, 1]
-        img = filters.gaussian(img)
-        img = filters.unsharp_mask(img)
-        img = exposure.equalize_hist(img)
-
-        x_moments = []
 
         for i in patches:
-            tmp_img = img[i[0, 0, 0]:i[4, 4, 0]+1, i[0, 0, 1]:i[4, 4, 1]+1]
-            tmp_img = np.nan_to_num(tmp_img)
-            norm_moments = measure.moments_normalized(measure.moments_central(tmp_img))
+            parameters.append(img[i[0, 0, 0]:i[4, 4, 0] + 1, i[0, 0, 1]:i[4, 4, 1] + 1].flatten())
 
-            hu_moments = measure.moments_hu(norm_moments)
-            hu_moments = np.where(hu_moments != 0, hu_moments, 10 ** -50)
-
-            for j in range(len(hu_moments)):
-                hu_moments[j] = -1 * np.copysign(1.0, hu_moments[j]) * np.log10(abs(hu_moments[j]))
-
-            parameters = np.append(hu_moments, np.var(tmp_img))
-
-            x_moments.append(parameters)
-
-        x_moments = np.array(x_moments)
-
-        return patches[:, 2, 2], x_moments
+        parameters = np.array(parameters)
+        return patches[:, 2, 2], parameters
